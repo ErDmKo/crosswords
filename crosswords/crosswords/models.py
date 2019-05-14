318775ac673efd28aecfc97a8ceee43f1cc34534
erdmko <erdmko@gmail.com>
2019-05-15 00:31:57 +0300

    Word randomaizer and console visualisation

--- crosswords/crosswords/models.py
+++ crosswords/crosswords/models.py
@@ -1,5 +1,35 @@
 from django.db import models
 
+class RandomManager(models.Manager):
+
+    def random(self, limit = 1):
+        count = self.count()
+        return self.raw("""
+            select * from {} TABLESAMPLE BERNOULLI ((%s * 100) / %s::decimal)
+        """.format(self.model._meta.db_table), [limit, count])
+
+    def setItem(self, field, y, x, val):
+        for i in range(y + 1):
+            if len(field) <= i:
+                field.append([])
+            for j in range(x + 1):
+                if len(field[i]) <= j:
+                    field[i].append('|  ')
+                if j == x and i == y:
+                    field[y][x] = '|{}'.format(val)
+                if j == x and j == len(field[i]) - 1:
+                    field[i][j] += '|'
+                    
+
+    def grid(self):
+        field = [[]]
+        self.setItem(field, 10, 9, '  ')
+        for index, letter in enumerate('Инея'):
+            self.setItem(field, 5, 3+index, '{}+'.format(letter))
+        for y in field:
+            print('\n', end='')
+            for x in y:
+                print(x, end='')
 
 class Word(models.Model):
     word = models.TextField(
@@ -10,6 +40,13 @@ class Word(models.Model):
     )
     multivalued = models.PositiveSmallIntegerField(
         verbose_name='Multivalued'
-    )
+    ) 
+
+    objects = models.Manager()
+    rnd = RandomManager()
+
+    def random(self, limit=1):
+        return self._meta
+
     def __str__(self):
         return self.word
