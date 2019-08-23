3bd97137a6464cfdbc12cbd8ff663f2f8a45552d
erdmko <erdmko@gmail.com>
2019-08-23 18:47:52 +0300

    Add pm2 conf from server

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    name: 'djangoAPI',
+    script: '.env/bin/uwsgi',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: ` --socket ./socket --wsgi-file ./crosswords/wsgi.py --chmod-socket=666 --enable-threads --chdir ./crosswords`,
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }]
+};
